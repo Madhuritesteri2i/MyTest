6494aec0dd895400d3007aa8b041a59636efcaef
madhu <madhu@LAPTOP-DAU977RG>
2022-06-15 23:06:32 +0530

    Test commit

---
+++ Automation/src/day1/Myfirstprogram.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Automation/src/module-info.java
@@ -0,0 +1,4 @@
+module Automation {
+	requires jakarta.activation;
+	requires selenium.api;
+}
